aa376ca5683d259951ad9e6b0cd6d9e1f95709ca
fuweiyuan <fuweiyuan@mujoy.com>
2020-06-04 16:54:11 +0800

    模板优化

--- js/main_index.js
+++ js/main_index.js
@@ -11,14 +11,14 @@ requirejs.config({
         /*******************/
 
         /***内部共用模块***/
-        ledu_topbar: im1_url+'source/js/base/topbar/ledu_topbar',//乐都顶部通栏
+        ledu_topbar: im1_url + 'source/js/base/topbar/ledu_topbar',//乐都顶部通栏
         login: im1_url + 'source/js/base/loginEject',//登录 文档地址：https://quqi.com/6/277793
         LDpaylayer: 'ledu/LDpaylayer/LDpaylayer', //支付宝，微信支付 文档地址：https://quqi.com/6/272366
         cnzz: 'ledu/cnzzsdk/cnzz',//友盟统计上报 文档地址：https://quqi.com/6/278578
         imsdk: 'ledu/imsdk/imsdk', // 客服入口文件
         hdsdk: 'ledu/hdsdk/hdsdk', // 活动通用上报模板 文档地址：https://quqi.com/6/279300        
         AMF: 'scripts/common/AMF', // 活动共用方法
-        weburl: 'scripts/common/weburl', // 活动共用链接
+        weburl: 'scripts/common/weburl', // 活动共用链接 
         IETest: 'scripts/IETest', //判断浏览器，在加载主页面文件时，必须先调用
         /*******************/
 
